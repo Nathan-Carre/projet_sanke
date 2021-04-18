e2e15ae1b68c8014b4c77a22fb14068ad338f8bd
Nathan-Carre <71774256+Nathan-Carre@users.noreply.github.com>
2021-04-18 19:20:52 +0200

    Update snake_final.py

--- snake_final.py
+++ snake_final.py
@@ -1,8 +1,8 @@
-####################################################
+###########################################################################################
 # Auteurs:(Prénom Nom)
-# Clément Maouche, Pauline Hosti, Maya Santini, Rania Saifi, Adeline Moutou, Nathan Carré
+#   Clément Maouche, Pauline Hosti, Maya Santini, Rania Saifi, Adeline Moutou, Nathan Carré
 # Groupe:
-# DLBI (Pauline Hosti: MP option Informatique)
-# dépot Github:
-#
-###################################################
+#   DLBI (Pauline Hosti: MP option Informatique)
+# Dépot Github:
+#   https://github.com/Nathan-Carre/projet_snake
+############################################################################################
